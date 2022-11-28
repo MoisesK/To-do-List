390a6c8b551bd7759904af90a3f9e50b6479b686
MoisesK <moises979@gmail.com>
2022-11-28 14:20:33 -0300

    feat: Métodos Getters e contrutor da model TO-DO e estrutura sql para o banco de dados

---
+++ db/to_do_list.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Tempo de geração: 28/11/2022 às 17:09
+-- Versão do servidor: 8.0.31
+-- Versão do PHP: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `to_do_list`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `to-do`
+--
+
+CREATE TABLE `to-do` (
+  `id` int NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `descript` varchar(300) NOT NULL,
+  `deadline` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `to-do`
+--
+ALTER TABLE `to-do`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `to-do`
+--
+ALTER TABLE `to-do`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
