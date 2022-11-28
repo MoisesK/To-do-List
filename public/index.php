4378eb48f1e1f4d3be78772d5e3ac374a3d386a5
Moisés Kalebe <mkdev@pop-os.localdomain>
2022-11-28 20:17:27 -0300

    feat:

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\App\Util\View;
+use App\Controllers\HomeController;
 use WilliamCosta\DatabaseManager\Database;
 use WilliamCosta\DotEnv\Environment;
 
@@ -11,4 +13,14 @@ Environment::load(__DIR__ . '/../');
 // Define as configurações de Banco de Dados
 Database::config(getenv('DB_HOST'), getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_PORT'));
 
-echo "sucesso";
+// Definir constante URL projeto
+define('URL', getenv('URL'));
+
+//Definir valor padrão de variaveis
+View::init([
+    'URL' => URL
+]);
+
+$home = new HomeController();
+
+$home->getHome();
