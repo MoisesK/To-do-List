1c0c0643b67773ecc717ab4b6c6e3d5ee23c43b2
MoisesK <moises979@gmail.com>
2022-12-01 16:10:55 -0300

    feat: método para criação de uma tarefa.

--- src/App/Http/Pages.php
+++ src/App/Http/Pages.php
@@ -6,9 +6,17 @@ namespace App\App\Http;
 use App\Controllers\HomeController;
 
 
+
 // ROTA HOME
 $obRouter->get('/', [
     function () {
         return new Response(200, HomeController::getHome());
+    }
+]);
+
+$obRouter->post('/', [
+    function () {
+        $request = new Request();
+        return new Response(200, HomeController::newTodo($request));
     }
 ]);
