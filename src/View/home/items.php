89d344082f7ad3766ccfa9c893a2cece656a4cf2
Moisés Kalebe <mkdev@pop-os.localdomain>
2022-12-04 13:37:52 -0300

    update: Ajustes na view de items.

--- src/View/home/items.php
+++ src/View/home/items.php
@@ -1,9 +1,13 @@
 <tbody>
     <tr class="table-light">
         <td class="d-flex">
-            {{descript}}
-            <a>X</a>
-            <a>D</a>
+            <h3>{{descript}}</h3>
+        </td>
+        <td>
+            <form method="post" class="d-flex justify-content-end">
+                <a class="btn btn-danger me-1" href="#" data-action="">D</a>
+                <button type="submit" value="{{id}}" class="btn btn-danger">E</button>
+            </form>
         </td>
     </tr>
 </tbody>
