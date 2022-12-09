1602fe881c48b023c37ac00bf5b73389badda93e
MoisesK <moises979@gmail.com>
2022-12-09 16:52:46 -0300

    refactor: Structure DB

--- db/to_do_list.sql
+++ db/to_do_list.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Tempo de geração: 01/12/2022 às 17:45
+-- Tempo de geração: 09-Dez-2022 às 19:49
 -- Versão do servidor: 8.0.31
--- Versão do PHP: 8.0.19
+-- versão do PHP: 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -13,18 +13,25 @@ SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHAtinyint*/
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `to_do_list`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `todoes`
+-- Estrutura da tabela `todoes`
+--
 
 CREATE TABLE `todoes` (
   `id` int NOT NULL,
-  `descript` varchar(300) NOT NULL,
-  `stats` int(0,1) NOT NULL
+  `title` varchar(200) NOT NULL,
+  `optional_descript` varchar(200) NOT NULL,
+  `stats` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -32,20 +39,20 @@ CREATE TABLE `todoes` (
 --
 
 --
--- Índices de tabela `todoes`
+-- Índices para tabela `todoes`
 --
 ALTER TABLE `todoes`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `todoes`
 --
 ALTER TABLE `todoes`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
